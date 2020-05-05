907f20c4d92ba43cb4ea93b8bbde8b29928d9c46
Gogi99 <goransilovo@elfak.rs>
2020-05-05 18:49:44 +0200

    Added Config Files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+ 
+module.exports = {
+  entry: './src/index.js',
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  
+  },
+  module:{
+      rules:[
+          {
+           test: /\.js$/,
+           exclude:/node_modules/,
+           use:'babel-loader'
+          },
+          {
+            test: /\.tsx?$/,
+            exclude:/node_modules/,
+            use:'ts-loader'
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader','css-loader']
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: 'file-loader'
+          }
+      ]
+  }
+};
